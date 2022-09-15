42d9bbca7572c13d56b2fcad20ea664a421299b7
SirScarlet <44670439+SirScarlet@users.noreply.github.com>
2022-09-15 10:33:46 +0300

    1

---
+++ config.js
@@ -0,0 +1,20 @@
+/*!
+ * Quara Theme by OllieJW (https://olliejw.me)
+ * License - https://olliejw.me/tos
+ */
+
+var server_name = "The Ghost Empire";
+var server_ip = "hypixel.net";
+var server_port = 25565;
+var ip_copied = "IP copied!";
+
+var firefly_count = 20;
+
+staff("MisterDef", "0b240959-eeec-45e9-a945-0946f9cea80d", "Владелец");
+staff("Sir_Scarlet", "b8c362fd-381e-45c3-ace5-d14dc626c133", "Куратор,Тех-Админ");
+staff("TheLisorML", "246b1f57-016e-4e7d-a63b-6d8ae21d5864", "Спонсор,Админ");
+
+vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
+vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");
+vote("Minecraft MP", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraft-mp.com/");
+vote("Top Minecraft Servers", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://topminecraftservers.org/");
